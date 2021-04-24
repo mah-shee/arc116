b06a5d921b249b2a6565fa03d2c20e5e1cd5b862
masa <masatoshi.sboard@gmail.com>
2021-04-24 21:00:14 +0900

    done contest

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,8 +1,60 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::collections::HashMap;
 #[fastout]
 fn main() {
     input! {
+        t: usize,
+        case: [usize; t],
     }
+    for i in 0..t {
+        let primes = trial_division(case[i]);
+        let mut total = 1;
+        let mut odd_only = 1;
+        for (key, value) in primes.iter() {
+            if key % 2 == 0 {
+                total *= value + 1;
+            } else {
+                odd_only *= value + 1;
+                total *= value + 1;
+            }
+        }
+        if total - odd_only == odd_only {
+            println!("Same");
+        } else if total - odd_only < odd_only {
+            println!("Odd");
+        } else {
+            println!("Even");
+        }
+    }
+}
+fn trial_division(mut n: usize) -> HashMap<usize, usize> {
+    let mut primes = HashMap::new();
+    let mut i = 2;
+
+    //  n を root n 以下の整数で割り切れるまで割っていく
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+            if primes.contains_key(&i) {
+                let x = primes.get_mut(&i).unwrap();
+                *x += 1;
+            } else {
+                primes.insert(i, 1);
+            }
+        }
+        i += 1;
+    }
+
+    // 最後にnが素数になっている場合はそれ自身も素因数に含めて終了
+    if n > 1 {
+        if primes.contains_key(&n) {
+            let x = primes.get_mut(&n).unwrap();
+            *x += 1;
+        } else {
+            primes.insert(n, 1);
+        }
+    }
+    primes
 }
--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,26 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n:usize,
+        mut a: [usize; n],
     }
+    a.sort();
+    let waru = 998244352;
+    let mut ans = 0;
+    for bit in 0..(1 << n - 1) {
+        let mut vector = Vec::new();
+        for i in 0..n {
+            if (bit & (1 << i)) == 1 {
+                vector.push(a[i]);
+                println!("{}", a[i]);
+            }
+        }
+        for k in vector.iter() {
+            println!("{}: {}", bit, k);
+        }
+        ans += vector[0] * vector[vector.len() - 1];
+        ans %= waru;
+        println!("{}", ans);
+    }
+    println!("{}", ans % waru);
 }
